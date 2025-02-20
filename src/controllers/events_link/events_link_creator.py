6d7f7965312567d92b4b7a110751624cadc60776
maurimnadal <maurim.nadal@gmail.com>
2025-02-20 17:28:25 -0300

    feat: finish controllers

---
+++ src/controllers/events_link/events_link_creator.py
@@ -0,0 +1,41 @@
+from src.http_types.http_request import HttpRequest
+from src.http_types.http_response import HttpResponse 
+from src.model.repositories.interfaces.eventos_link_repository import EventosLinkRepositoryInterface
+
+
+class EventsLinkCreator:
+    def __init__(self, eventos_link_repo: EventosLinkRepositoryInterface):
+        self.__events_link_repo = eventos_link_repo
+        
+    def create(self, http_request: HttpRequest) -> HttpResponse:
+        event_link_info = http_request.body["data"]
+        event_id = event_link_info["event_id"]
+        sub_id = event_link_info["sub_id"]
+
+        self.__check_event_link(event_id, sub_id)
+        new_link = self.__create_event_link(event_id, sub_id)
+        return self.__format_response(new_link, event_id, sub_id)
+
+    def __check_event_link(self, event_id: int, sub_id: int) -> None:
+        response = self.__events_link_repo.select_event_link(event_id, sub_id)
+        if response:
+            raise Exception("Link already exists")
+        
+    
+    def __create_event_link(self, event_id: int, sub_id: int) -> str:
+        new_link = self.__events_link_repo.insert(event_id, sub_id)
+        return new_link 
+    
+    def __format_response(self, new_link:str, event_id:int, sub_id:int) -> HttpResponse:
+        body = {
+            "data": {
+                "Type": "Event Link",
+                "count": 1,
+                "attributes": {
+                    "link": new_link,
+                    "event_id": event_id,
+                    "sub_id": sub_id,
+                }
+            },
+            "status_code": 201
+        }
---
+++ src/controllers/subscribers/subs_manager.py
@@ -0,0 +1,60 @@
+from src.http_types.http_request import HttpRequest
+from src.http_types.http_response import HttpResponse
+from src.model.repositories.interfaces.subscribers_repository import SubscribersRepositoryInterface
+
+class SubscriberManager:
+    def __init__(self, subs_repo: SubscribersRepositoryInterface):
+        self.__subs_repo = subs_repo
+        
+    def get_subscribers_by_link(self, http_request: HttpRequest) -> HttpResponse:
+        link = http_request.param["link"]
+        event_id = http_request.param["event_id"]
+        subs = self.__subs_repo.select_subs_by_link(link, event_id)
+        
+        return self.__format_subs_by_link(subs)
+    
+    def get_event_ranking(self, http_request: HttpRequest) -> HttpResponse:
+        event_id = http_request.param["event_id"]
+        event_ranking = self.__subs_repo.get_ranking(event_id)
+        
+        return self.__format_event_ranking(event_ranking)
+    
+    def __format_subs_by_link(self, subs: list) -> HttpResponse:
+        formatted_sub = []
+        for sub in subs:
+            formatted_sub.append(
+                {
+                    "nome": sub.nome,
+                    "email": sub.email,
+                }
+            )
+        return HttpResponse(
+            body={
+                "data": {
+                    "Type" : "Subscriber",
+                    "count": len(formatted_sub),
+                    "subscribers" : formatted_sub
+                }
+            }
+        )
+        
+    def __format_event_ranking(self, event_ranking: list) -> HttpResponse:
+        formatted_event_ranking = []
+        for position in event_ranking:
+            formatted_event_ranking.append(
+                {
+                    "nome": position.link,
+                    "email": position.total,
+                }
+            )
+        return HttpResponse(
+            body={
+                "data": {
+                    "Type" : "Ranking",
+                    "count": len(formatted_event_ranking),
+                    "ranking" : formatted_event_ranking
+                }
+            }
+        )
+        
+    
