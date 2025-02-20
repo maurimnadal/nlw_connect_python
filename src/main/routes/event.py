3c4ee098c9881272d7498ca797c4b5ccad404c76
maurimnadal <maurim.nadal@gmail.com>
2025-02-20 15:37:17 -0300

    feat: Implementing controllers

--- src/main/routes/event.py
+++ src/main/routes/event.py
@@ -1,7 +1,11 @@
 from flask import Blueprint, jsonify, request
-from src.http_types.http_response import HttpResponse
+
 from src.http_types.http_request import HttpRequest
+
 from src.validators.events_creator_validator import events_creator_validator
+
+from src.controllers.events.events_creator import EventsCreator
+from src.model.repositories.eventos_repository import EventosRepository
 
 event_route_bp = Blueprint('event_route', __name__)
 
@@ -10,7 +14,10 @@ def create_new_event():
     events_creator_validator(request)
     http_request = HttpRequest(body=request.json)
     
-    http_response = HttpResponse(body={"estou": "aqui"}, status_code=201)
+    eventos_repo = EventosRepository()
+    events_creator = EventsCreator(eventos_repo)
+    
+    http_response = events_creator.create(http_request)
     
     return jsonify(http_response.body), http_response.status_code
 
