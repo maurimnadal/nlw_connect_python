a5d5dcb695763793e6f62dcf107ec41e2c69346f
maurimnadal <maurim.nadal@gmail.com>
2025-03-27 16:01:16 -0300

    delete: removing case.py

--- case.py
+++
@@ -1,9 +0,0 @@
-class MinhaClasse:
-    def __enter__(self):
-        print("Entrei")
-        
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        print("Sai")
-        
-with MinhaClasse() as mc:
-    print("Estou aqui dentro")
